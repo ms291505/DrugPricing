bae67c5edd0ca9e5d0925cab535d316196c15e80
ms291505 <clang-anchor.0n@icloud.com>
2026-08-15 06:26:02 -0400

    ui update
    mobile drawer FAB now matches behavior with the tab context fab.

--- client/src/Components/Workspace/MobileDrawerFab.tsx
+++ client/src/Components/Workspace/MobileDrawerFab.tsx
@@ -4,6 +4,7 @@ import { Fab, } from "@mui/material";
 import { alpha } from "@mui/material/styles";
 import Menu from "@mui/icons-material/Menu";
 import { useWorkspaceContext } from "../../Context/WorkspaceContext.tsx";
+import useScrolled from "../../hooks/useScrolled.ts";
 
 export default function MobileDrawerFab() {
   const { mobileDrawerIsOpen, setMobileDrawerIsClosing, setMobileDrawerIsOpen } = useWorkspaceContext();
@@ -16,6 +17,8 @@ export default function MobileDrawerFab() {
   };
 
   const { opacity, drawerWidth } = CONSTANT;
+
+  const scrolled = useScrolled();
 
   return (
 
@@ -30,13 +33,19 @@ export default function MobileDrawerFab() {
         left: 16,
         transform: mobileDrawerIsOpen ? `translateX(${drawerWidth}px)` : "translateX(0)",
         transition: (theme) =>
-          theme.transitions.create("transform", {
-            easing: mobileDrawerIsOpen ? theme.transitions.easing.easeOut : theme.transitions.easing.sharp,
-            duration: mobileDrawerIsOpen ? theme.transitions.duration.enteringScreen : theme.transitions.duration.leavingScreen,
-          }),
+          [
+            theme.transitions.create("transform", {
+              easing: mobileDrawerIsOpen ? theme.transitions.easing.easeOut : theme.transitions.easing.sharp,
+              duration: mobileDrawerIsOpen ? theme.transitions.duration.enteringScreen : theme.transitions.duration.leavingScreen,
+            }),
+            theme.transitions.create("background-color", {
+              easing: theme.transitions.easing.sharp,
+              duration: theme.transitions.duration.shortest,
+            }),
+          ].join(", "),
         zIndex: (theme) => theme.zIndex.drawer + 1,
         display: { xs: "flex", md: "none" },
-        bgcolor: (theme) => alpha(theme.palette.primary.main, opacity),
+        bgcolor: (theme) => alpha(theme.palette.primary.main, scrolled ? opacity : 1),
         backdropFilter: "blur(10px)",
         WebkitBackdropFilter: "blur(10px)",
       }}
